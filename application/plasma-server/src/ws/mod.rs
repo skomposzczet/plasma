76a9cb71e807aa4c3c62d1166099906c91167c20
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-10-14 07:55:59 +0200

    add poc websocket connection

--- application/plasma-server/src/ws/mod.rs
+++ application/plasma-server/src/ws/mod.rs
@@ -1,10 +1,13 @@
 pub mod clients;
 
 use std::sync::Arc;
-use warp::{Filter, reject::Rejection, reply::Json};
+use futures::{StreamExt, SinkExt, TryFutureExt};
+use tokio::sync::mpsc;
+use warp::{Filter, reject::Rejection, reply::Reply, ws::WebSocket};
 use crate::{model::Db, server::with_auth, ClientsHandle};
+use tokio_stream::wrappers::UnboundedReceiverStream;
 
-pub fn ws_paths(db: Arc<Db>, clients: ClientsHandle) -> impl Filter<Extract = (impl warp::Reply,), Error = Rejection> + Clone {
+pub fn ws_paths(db: Arc<Db>, clients: ClientsHandle) -> impl Filter<Extract = (impl Reply,), Error = Rejection> + Clone {
     let with_db = warp::any()
         .map(move || db.clone());
     let with_clients = warp::any()
@@ -21,7 +24,76 @@ pub fn ws_paths(db: Arc<Db>, clients: ClientsHandle) -> impl Filter<Extract = (i
     chat
 }
 
-async fn handle(ws: warp::ws::Ws, db: Arc<Db>, clients: ClientsHandle, oid: String) -> Result<Json, Rejection> {
-    todo!()
+async fn handle(ws: warp::ws::Ws, db: Arc<Db>, clients: ClientsHandle, oid: String) -> Result<impl Reply, Rejection> {
+    Ok(ws.on_upgrade(move |socket| user_connected(socket, db.clone(), clients.clone(), oid)))
 }
 
+async fn user_connected(mut socket: WebSocket, db: Arc<Db>, clients: ClientsHandle, oid: String) {
+    debug!("User connected: {}", oid);
+
+    for i in 0..5 {
+        if socket.send(warp::filters::ws::Message::text(format!("hello {}", i))).await.is_err() {
+            error!(":< dc {}", oid);
+            return;
+        }
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+    }
+
+    let (mut user_ws_tx, mut user_ws_rx) = socket.split();
+    let (tx, rx) = mpsc::unbounded_channel();
+    let mut rx = UnboundedReceiverStream::new(rx);
+
+    clients.write().await.add_client(oid.clone(), tx);
+
+    tokio::task::spawn(async move {
+        while let Some(message) = rx.next().await {
+            user_ws_tx
+                .send(message)
+                .unwrap_or_else(|e| {
+                    error!("websocket err: {}", e);
+                })
+                .await;
+        }
+    });
+
+    while let Some(result) = user_ws_rx.next().await {
+        let msg = match result {
+            Ok(msg) => msg,
+            Err(e) => {
+                error!("websocket error: id={}, error={}", oid, e);
+                break;
+            },
+        };
+        user_message(&oid, msg, clients.clone()).await;
+    }
+    disconnect_user(&oid, clients.clone()).await;
+}
+
+async fn user_message(oid: &str, msg: warp::filters::ws::Message, clients: ClientsHandle) {
+    let msg = if let Ok(s) = msg.to_str() {
+        s
+    } else {
+        error!("non text msg");
+        return;
+    };
+    let msg = format!("<{}>: {}", oid, msg);
+
+    match clients.write().await.get_client(oid) {
+        None => {
+            error!("no client for {}", oid);
+            return
+        },
+        Some(client) => {
+            let res = client.send(warp::filters::ws::Message::text(msg));
+            if res.is_err() {
+                error!("User disconnected {}", oid);
+                return;
+            }
+        }
+    }
+}
+
+async fn disconnect_user(oid: &str, clients: ClientsHandle) {
+    debug!("User disconnected: {}", oid);
+    clients.write().await.remove_client(oid);
+}
