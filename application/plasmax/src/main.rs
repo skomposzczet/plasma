ef38b63e0f0a2fafc8581b9aa9c2b2c9295c7fe6
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2023-10-14 08:15:06 +0200

    add basic cli for loggin / registering

--- application/plasmax/src/main.rs
+++ application/plasmax/src/main.rs
@@ -1,3 +1,48 @@
+use clap::{Parser, Subcommand};
+use std::io::Write;
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Login to account
+    Login {
+        #[arg(short, long, help="Mail to login with")]
+        mail: String,
+    },
+
+    /// Register new account
+    Register {
+        #[arg(short, long, help="Mail to register with")]
+        mail: String,
+        #[arg(short, long, help="Unique username")]
+        username: String,
+    },
+}
+
+fn get_password() -> String {
+    print!("Password: ");
+    std::io::stdout().flush().unwrap();
+    let pw = rpassword::read_password().unwrap();
+    pw
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Some(Commands::Login { mail } ) => {
+            let pw = get_password();
+            println!("ml={}, pw={}", mail, pw);
+        },
+        Some(Commands::Register { mail, username } ) => {
+            let pw = get_password();
+            println!("ml={}, un={}, pw={}", mail, username, pw);
+        },
+        None => {},
+    }
 }
